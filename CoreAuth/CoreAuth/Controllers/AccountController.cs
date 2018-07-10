5ae36613942ccf0e002db8ca72c27aae1fd449e6
nkarg <r.cadenas.edu@gmail.com>
2018-07-10 20:35:48 -0300

    Login caqueño

---
+++ CoreAuth/CoreAuth/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CoreAuth.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAuth.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var auth = HttpContext.User.Identity.IsAuthenticated;
+
+            if (auth)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, model.UserName),
+                new Claim(ClaimTypes.Email, model.UserName)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+            
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ CoreAuth/CoreAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
